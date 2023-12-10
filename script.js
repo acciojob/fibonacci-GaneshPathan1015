84daecd455e646bb0151804f65e58da5608c7536
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-10 22:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	let a=0;
+	let b=1;
+	let c=0;
+	let val = 1;
+	while(val<num){
+		
+		a=b;
+		b=c;
+		c=a+b;
+		val++;
+	}
+	console.log(c);
 }
 
 module.exports = fibonacci;
